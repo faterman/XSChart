bf89c196f45dc06ee8e36de6cd3c32dd320c74e7
faterman <xxyformobile@163.com>
2017-01-17 19:44:33 +0800

    小修改

--- XSChartDemo/XSChartDemo/ViewController.m
+++ XSChartDemo/XSChartDemo/ViewController.m
@@ -20,7 +20,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     XSLineChart * lineChart = [[XSLineChart alloc] initWithFrame:CGRectMake(0, 100, SCREEN_WIDTH, 200.0)];
     [lineChart setXLabels:@[@"12/1",@"12/2",@"12/3",@"12/4",@"12/5"]];
-    NSArray * dataArray = @[@5.1, @3.1, @6.4, @7.2, @10.2];
+    NSArray * dataArray = @[@2, @8, @1.3, @7.6, @2];
     XSLineChartData *data = [XSLineChartData new];
     data.itemCount = lineChart.xLabels.count;
     data.getData = ^(NSUInteger index) {
@@ -30,8 +30,6 @@
     lineChart.showSmoothLines = YES;
     lineChart.chartData = data;
     [lineChart strokeChart];
-    
-    
     [self.view addSubview:lineChart];
 }
 
